d5990b807ff253766078345f6582ded95f6ce2f0
Tim Liu <qing.liu@uwaterloo.ca>
2019-07-24 20:43:29 -0400

    Upload new file

---
+++ Minion.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Tim Liu on 2019-07-22.
+//
+
+#include "Minion.h"
+int Minion::get_attack() {
+    return attack;
+}
+int Minion::get_defence() {
+    return defence;
+}
+bool Minion::has_activated() {
+    return has_active;
+}
+bool Minion::get_activated_cost() {
+    return activated_cost;
+}
+bool Minion::get_action_left() {
+    return action_number - action_performed;
+}
+bool Minion::check_death() {
+    if (get_defence() == 0) {
+        setState(Effect{EffectType::MLC, 0, 0, CollectionType::GRAVE, 0});
+        notifyObservers();
+    }
+}
