e52947d2502692e2eb038ace1cff7cd1acbde62e
Tim Liu <qing.liu@uwaterloo.ca>
2019-07-24 20:45:25 -0400

    Upload new file

---
+++ Subject.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Tim Liu on 2019-07-22.
+//
+
+#include "Subject.h"
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::attach(Observer<InfoType, StateType> *o) {
+    observers.emplace_back(o);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::notifyObservers() {
+    for (auto &ob : observers) ob->notify(*this);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::setState(StateType newS) { state = newS; }
+
+template <typename InfoType, typename StateType>
+StateType Subject<InfoType, StateType>::getState() const { return state; }
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::detach(Observer<InfoType, StateType> *o) {
+    for (auto i = observers.begin(); i != observers.end(); i++) {
+        if (i == o) {
+            observers.erase(i);
+        }
+    }
+}
