cee96930e276bff7f928048846602e0c3415c2fb
Tim Liu <qing.liu@uwaterloo.ca>
2019-07-24 20:44:35 -0400

    Upload new file

---
+++ Subject.h
@@ -0,0 +1,42 @@
+//
+// Created by Tim Liu on 2019-07-22.
+//
+#include <vector>
+#include <iostream>
+
+#ifndef SORCERY_SUBJECT_H
+#define SORCERY_SUBJECT_H
+
+template <typename InfoType, typename StateType> class Observer;
+
+template <typename InfoType, typename StateType> class Subject {
+    std::vector<Observer<InfoType, StateType>*> observers;
+    StateType state;
+protected:
+    void setState(StateType newS);
+public:
+    void attach(Observer<InfoType, StateType> *o);
+    void detach(Observer<InfoType, StateType> *o);
+    void notifyObservers();
+    virtual InfoType getInfo() const = 0;
+    StateType getState() const;
+};
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::attach(Observer<InfoType, StateType> *o) {
+    observers.emplace_back(o);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::notifyObservers() {
+    for (auto &ob : observers) ob->notify(*this);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::setState(StateType newS) { state = newS; }
+
+template <typename InfoType, typename StateType>
+StateType Subject<InfoType, StateType>::getState() const { return state; }
+
+
+#endif //SORCERY_SUBJECT_H
