d3a1c4ab2e0b083cb630e0da04df03014b033141
Tim Liu <q256liu@uwaterloo.ca>
2019-07-26 21:21:37 -0400

    game class

---
+++ game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tim Liu on 2019-07-26.
+//
+
+#include "game.h"
---
+++ game.h
@@ -0,0 +1,16 @@
+//
+// Created by Tim Liu on 2019-07-26.
+//
+
+#ifndef CODES_GAME_H
+#define CODES_GAME_H
+
+#include "Collection.h"
+
+
+class Game {
+
+};
+
+
+#endif //CODES_GAME_H
