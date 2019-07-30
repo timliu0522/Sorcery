0a78b690fbbef48e48020b84e3d6289327694d4a
Kevinwen1999 <cybergodzilla1999@gmail.com>
2019-07-30 01:25:12 -0400

    Fuck my life

--- Subject.cpp
+++ Subject.cpp
@@ -1,36 +1,3 @@
 //
 // Created by Tim Liu on 2019-07-22.
 //
-
-#include "Subject.h"
-
-template <typename InfoType, typename StateType>
-void Subject<InfoType, StateType>::attach(std::shared_ptr<Observer<InfoType, StateType>> o) {
-    observers.emplace_back(o);
-}
-
-template <typename InfoType, typename StateType>
-void Subject<InfoType, StateType>::notifyObservers() {
-    for (auto &ob : observers) ob->notify(*this);
-}
-
-template <typename InfoType, typename StateType>
-void Subject<InfoType, StateType>::setState(StateType newS) { state = newS; }
-
-template <typename InfoType, typename StateType>
-void Subject<InfoType, StateType>::setInfo(InfoType newI) { info = newI; }
-
-template <typename InfoType, typename StateType>
-StateType Subject<InfoType, StateType>::getState() const { return state; }
-
-template <typename InfoType, typename StateType>
-void Subject<InfoType, StateType>::detach(std::shared_ptr<Observer<InfoType, StateType>> o) {
-    for (auto i = observers.begin(); i != observers.end(); i++) {
-        if (i == o) {
-            observers.erase(i);
-        }
-    }
-}
-
-template <typename InfoType, typename StateType>
-InfoType Subject<InfoType, StateType>::getInfo() const { return info; }
--- Subject.h
+++ Subject.h
@@ -25,4 +25,36 @@ public:
     // virtual ~Subject() = 0;
 };
 
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::attach(std::shared_ptr<Observer<InfoType, StateType>> o) {
+    observers.emplace_back(o);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::notifyObservers() {
+    for (auto &ob : observers) ob->notify(*this);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::setState(StateType newS) { state = newS; }
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::setInfo(InfoType newI) { info = newI; }
+
+template <typename InfoType, typename StateType>
+StateType Subject<InfoType, StateType>::getState() const { return state; }
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::detach(std::shared_ptr<Observer<InfoType, StateType>> o) {
+    for (auto i = observers.begin(); i != observers.end(); i++) {
+        if (i == o) {
+            observers.erase(i);
+        }
+    }
+}
+
+template <typename InfoType, typename StateType>
+InfoType Subject<InfoType, StateType>::getInfo() const { return info; }
+
 #endif //SORCERY_SUBJECT_H
